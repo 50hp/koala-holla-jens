dff068260970d203e1e784191628e365cd3b6def
Majid Igale <majidigale18@gmail.com>
2023-05-18 16:11:46 -0500

    POST is created in server, and has an event listener and event handler on client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,30 +2,66 @@ console.log( 'js' );
 
 $( document ).ready( function(){
   console.log( 'JQ' );
+  $('#addKoala').on('click', postKoala)
+  
+  
+  
+  
   // Establish Click Listeners
-  setupClickListeners()
+  // setupClickListeners()
   // load existing koalas on page load
-  getKoalas();
+  // getKoalas();
 
 }); // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+
+function postKoala(){
+  console.log('click works!')
+  let kInputs = {
+    kName: $('#nameIn').val(),
+    kAge: $('#ageIn').val(),
+    kGender: $('#genderIn').val(),
+    kReadyTrans: $('#readyForTransferIn').val(),
+    kNotes: $('#notesIn'),
+
+
+  }
+$.ajax({
+  method: 'POST',
+  url: '/koalas',
+  data: kInputs
+}).then(function(response) {
+  console.log(response);
+  
+}).catch(function(error) {
+  console.log('error in artist post', error); 
+  alert('Error adding artist. Please try again later.')       
+});
+
 }
+
+
+
+
+
+
+// function setupClickListeners() {
+//   $( '#addButton' ).on( 'click', function(){
+//     console.log( 'in addButton on click' );
+//     // get user input and put in an object
+//     // NOT WORKING YET :(
+//     // using a test object
+//     let koalaToSend = {
+//       name: 'testName',
+//       age: 'testName',
+//       gender: 'testName',
+//       readyForTransfer: 'testName',
+//       notes: 'testName',
+//     };
+//     // call saveKoala with the new obejct
+//     saveKoala( koalaToSend );
+//   }); 
+// }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -8,7 +8,22 @@ const pool = require('../modules/pool');
 
 
 // POST
+router.post('/', (req, res) => {
+    const newKoala = req.body
 
+    const queryText = `
+    INSERT INTO "koala_library" ("name", "age", "gender", "readyToTransfer", "notes")
+    VALUES ($1,$2,$3,$4,$5);`
+
+    const values = [newKoala.name,newKoala.age,newKoala.gender,newKoala.readyToTransfer,newKoala.notes,];
+    pool.query(queryText,values)
+    .then(result => {
+        
+}).catch(error => {
+            console.log('Query text is:', queryText, 'Our Error is:', error)
+            res.sendStatus(500)
+        })
+});
 
 // PUT
 
