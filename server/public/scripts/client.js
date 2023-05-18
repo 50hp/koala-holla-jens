8ec1e42248d828774591c3392000d75e35972dab
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-18 15:27:22 -0500

    created a getKoals function onthe client and a render to dom function on the client.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,29 +9,65 @@ $( document ).ready( function(){
 
 }); // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
-}
+// function setupClickListeners() {
+//   $( '#addButton' ).on( 'click', function(){
+//     console.log( 'in addButton on click' );
+//     // get user input and put in an object
+//     // NOT WORKING YET :(
+//     // using a test object
+//     let koalaToSend = {
+//       name: 'testName',
+//       age: 'testName',
+//       gender: 'testName',
+//       readyForTransfer: 'testName',
+//       notes: 'testName',
+//     };
+//     // call saveKoala with the new obejct
+//     saveKoala( koalaToSend );
+//   }); 
+// }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
-  
-} // end getKoalas
+
+
+    $.ajax({
+        method: 'GET',
+        url: '/koalas',
+    }).then((response) => {
+        renderToDom(response);
+    }).catch((err) => {
+        alert('koalas get request failed');
+        console.log('koals get request failed', err);
+    });
+
+}// end getKoalas
+
+function renderToDom(array){
+        console.log('in renderToDom');
+    let readyToggle = '<button class="transferBtn">Not Ready</button>';
+    for ( let item of array) {
+
+        if(item.readToTransfer ==='true'){
+            readyToggle = '<button class="transferBtn">Ready for Transfer</button>';
+        }
+
+        $('#viewKoalas').append(`
+
+            <tr>
+                <td class="koalaId">${item.id}</td>
+                <td class="koalaName">${item.name}</td>
+                <td class="koalaAge">${item.age}</td>
+                <td class="koalaGender">${item.gender}</td>
+                <td class="koalareadyToTransfer">${item.readyToTransfer}</td>
+                <td class="koalanotes">${item.notes}</td>
+                <td class="transferBtn">${readyToggle}</td>
+                <td class="remove"><button class="removeBtn">Remove</button></td>
+            </tr>
+        `);
+    }
+
+}
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
