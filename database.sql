5412d6e81b6ff1e0941b32e771aeedf3f2c1e48a
Adam Oberle <oberle.adam@gmail.com>
2023-05-18 15:30:51 -0500

    database initial commit - database.sql created, pool.js created and sourced

---
+++ database.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "koala_library" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(80) NOT NULL,
+    "age" INTEGER NOT NULL,
+    "gender" VARCHAR(1) NOT NULL,
+    "readyToTransfer" BOOLEAN DEFAULT 'false',
+    "notes" VARCHAR (80)
+);
+
+
+INSERT INTO "koala_library" ("name", "age", "gender", "readyToTransfer", "notes") 
+VALUES ('Scotty', 4, 'M', 'true', 'Born in Guatemala'),
+('Jean', 5, 'F', 'true', 'Allergic to lots of lava'),
+('Ororo', 7, 'F', 'false', 'loves listening to Paula Abdul'),
+('Logan', 15, 'M', 'false', 'Loves the sauna'),
+('Charlie', 9, 'M', 'true', 'Favorite band is Nirvana'),
+('Betsy', 4, 'F', 'true', 'Has a pet iguana');
